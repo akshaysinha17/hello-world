0e3da30d2cee94590947800a0ea0cb9b36436229
akshaysinha17 <69646451+akshaysinha17@users.noreply.github.com>
2020-08-13 11:01:23 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Akshay Sinha");
+		System.out.println("I am a junior at Milpitas High School");
+		System.out.println("I play basketball");
+		System.out.println("I also like playing video games");
+		System.out.println("I want to go to a UC for college");
+	}
+}
